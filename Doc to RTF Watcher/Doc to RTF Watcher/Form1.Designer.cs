2cddec57d62c554f3f7cfb621710ee75e6806ae8
RoxtonLabs <roxtonlabs@gmail.com>
2015-12-11 19:02:11 +0000

    First draft

---
+++ Doc to RTF Watcher/Doc to RTF Watcher/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Doc_to_RTF_Watcher
+{
+    partial class WatcherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dirBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.browseButton);
+            this.groupBox1.Controls.Add(this.dirBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter the folder to watch:";
+            // 
+            // dirBox
+            // 
+            this.dirBox.Location = new System.Drawing.Point(7, 20);
+            this.dirBox.Name = "dirBox";
+            this.dirBox.Size = new System.Drawing.Size(215, 20);
+            this.dirBox.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(228, 18);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(240, 72);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // WatcherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 106);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "WatcherForm";
+            this.Text = "Doc to RTF Watcher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox dirBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+    }
+}
+
---
+++ Doc to RTF Watcher/Doc to RTF Watcher/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //For the directory
+using Word = Microsoft.Office.Interop.Word; //Right-click solution, Add>Reference>Microsoft word 14.0 Object Library
+using System.Timers;    //For the timer
+
+namespace Doc_to_RTF_Watcher
+{
+    public partial class WatcherForm : Form
+    {
+        //Watcher variables
+        Boolean watching = false;
+        Boolean processing = false;
+        string[] oldFiles;
+        string[] newFiles;
+        string[] emptyFiles;
+        System.Timers.Timer watchTimer = new System.Timers.Timer(5000);
+
+
+        public WatcherForm()
+        {
+            InitializeComponent();
+            watchTimer.Enabled = false;
+            watchTimer.Elapsed += new ElapsedEventHandler(watchTimer_Elapsed);
+        }
+
+        void watchTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {   //This gets called once every five seconds while the timer is running
+            //Creates a list of all the .doc and .docx files in the folder. If they're different, then we have new files to process
+            newFiles = Directory.GetFiles(dirBox.Text, "*.doc*");
+            if (!newFiles.SequenceEqual(oldFiles))
+            {
+                MessageBox.Show("Change detected!");
+                oldFiles = newFiles;
+            }
+        }
+
+        void startWatcher()
+        {   //starts the timer
+            //assumes that the input folder should be empty, meaning that we'll handle blank files well
+            oldFiles = Directory.GetFiles(dirBox.Text, "*.doc*");
+            watchTimer.Enabled = true;
+            watchTimer.Stop();
+            watchTimer.Start();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {   //Allow the user to GUI their way to the folder if they don't have a keyboard
+            DialogResult result = folderBrowser.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                dirBox.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startWatcher();
+        }
+
+        private void runConversion()
+        {   //Converts all the .doc and .docx files in the folder into RTFs
+
+            //Create an output folder if it doesn't exist
+            if (!Directory.Exists(dirBox.Text + @"\Output"))
+            {
+                Directory.CreateDirectory(dirBox.Text + @"\Output");
+            }
+
+            //First get all the files in the folder that we need to convert
+            string[] fileNames = Directory.GetFiles(dirBox.Text, "*.doc*");
+
+            //Now create word and get transforming
+            Word.Application wordApp = new Word.Application();
+            wordApp.Visible = true;
+            Word.Document wordDoc;
+            object unknown = Type.Missing;
+
+            //Start Word and open the document
+            foreach (string fileName in fileNames)
+            {   //Open the file
+                Console.WriteLine(fileName);
+                object newName = dirBox.Text + @"\Output\" + Path.GetFileNameWithoutExtension(fileName) + ".rtf";
+                object format = Word.WdSaveFormat.wdFormatRTF;  //Have to set this to an instance of an object; we can't just use the reference in SaveAs2.
+                try
+                {   //This returns an error even though the file is fine
+                    wordDoc = wordApp.Documents.Open(fileName);
+                    wordDoc.SaveAs2(newName, format);
+                    wordDoc.Close();
+                }
+                catch (Exception) { }
+            }
+
+            //Close Word and release the objects
+            wordApp.Quit();
+            releaseObject(wordApp);
+            wordDoc = null; //We have to do this because of the try loop above
+            releaseObject(wordDoc);
+
+            //Finally, delete the files we've converted
+            foreach (string fileName in fileNames)
+            {
+                try
+                {
+                    File.Delete(fileName);
+                }
+                catch (Exception) { }
+            }
+        }
+
+        //Release interop objects
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception x)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();   //Call in the binman
+            }
+        }
+    }
+}

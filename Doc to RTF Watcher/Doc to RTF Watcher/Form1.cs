7183fa563a235d82c4f7140909f040ee4ae1800d
RoxtonLabs <roxtonlabs@gmail.com>
2015-12-15 16:06:41 +0000

    Attempt to stop locked files causing ouroboroi
    If all the files in the folder were locked or had other permission issues (e.g. were created by another on a shared drive with some odd permission settings), the code would stick on checking (status=2) forever. Ultimately, the IsFileLocked function is too sensitive to be used and is in any case unnecessary because the code always treats any files as new.
    
    Currently the old code is still present but is unused; until proper testing has been done I shan't remove it entirely.

--- Doc to RTF Watcher/Doc to RTF Watcher/Form1.cs
+++ Doc to RTF Watcher/Doc to RTF Watcher/Form1.cs
@@ -19,7 +19,7 @@ namespace Doc_to_RTF_Watcher
         int status = 0; //0 = off, 1 = running, 2 = checking, 3 = processing
         string[] oldFiles;
         string[] newFiles;
-        string[] emptyFiles;
+        List<string> toConvert = new List<string>();
         System.Timers.Timer watchTimer = new System.Timers.Timer(5000);
          
         public WatcherForm()
@@ -42,33 +42,39 @@ namespace Doc_to_RTF_Watcher
                     newFiles = Directory.GetFiles(dirBox.Text, "*.doc*");
                     if (!newFiles.SequenceEqual(oldFiles))
                     {   //So we have new files to process.
+                        Console.WriteLine("New files found!");
                         //Before we do the processing, we want to check that all the files can be read
-                        status = 2; //Update status flag to show that we're checking the files
+                        status = 3; //Update status flag to show that we're checking the files
                                     //This will cause us to wait for a second or so, which is fine
                         oldFiles = newFiles;
-                    }
-                    break;
-                case 2: //Checking
-                    //Check all files in the folder, not just the ones that triggered change
-                    string[] checkFiles = Directory.GetFiles(dirBox.Text, "*.doc.*");
-                    Boolean readyToGo = true;
-                    foreach (string fileName in checkFiles)
-                    {
-                        FileInfo fi = new FileInfo(fileName);
-                        if (isFileLocked(fi))
-                        {   //If a single file is locked then there's no point going any further; we stop and wait for the next tick
-                            readyToGo = false;
-                            break;  //No point checking them all
-                        }
-                    }
-                    if (readyToGo)
-                    {   //If all the files were unlocked, then move on to processing
-                        status = 3;
                         runConversion();
                     }
                     break;
+                case 2: //Checking
+                    Console.WriteLine("Checking files...");
+                    //Check all files in the folder, not just the ones that triggered change
+                    string[] checkFiles = Directory.GetFiles(dirBox.Text, "*.doc.*");
+                    //Boolean readyToGo = true;
+                    foreach (string fileName in checkFiles)
+                    {
+                        Console.WriteLine("Adding " + fileName + " to list");
+                        FileInfo fi = new FileInfo(fileName);
+                        if (!isFileLocked(fi))
+                        {   //If a file is unlocked, add it to the list of files to convert
+                            toConvert.Add(fileName);
+                            Console.WriteLine("LOCKED: " + fileName);
+                        }
+                    }
+                    Console.WriteLine("All files added, list is:");
+                    Console.WriteLine(toConvert);
+                    Console.WriteLine("Count = " + toConvert.Count);
+                    //Now we've checked all the files, send them off to be processed
+                    if (toConvert.Count>0)
+                    {
+                        status = 3;
+                    }
+                    break;
                 case 3: //Processing
-                        //Don't check for new files as that's done by the startWatcher method
                     break;
             }
         }
@@ -82,9 +88,9 @@ namespace Doc_to_RTF_Watcher
             FileStream stream = null;
             try
             {
-                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
             }
-            catch (IOException)
+            catch (Exception)
             {   //The file is unavailable for some reason
                 return true;
             }
@@ -133,9 +139,21 @@ namespace Doc_to_RTF_Watcher
             switch (status)
             {
                 case 0: //Inactive
-                    startWatcher(); //Start the watcher
-                    startButton.Text = "Stop";
-                    dirBox.Enabled = false;
+                        //Create an output folder if it doesn't exist
+                    if (!Directory.Exists(dirBox.Text))
+                    {   //Check that the directory exists and stop if it doesn't
+                        MessageBox.Show("Could not find " + dirBox.Text + ". Please enter a valid directory and try again.");
+                    }
+                    else
+                    {   //If the output directory doesn't exist then create it.
+                        if (!Directory.Exists(dirBox.Text + @"\Output"))
+                        {
+                            Directory.CreateDirectory(dirBox.Text + @"\Output");
+                        }
+                        startWatcher(); //Start the watcher
+                        startButton.Text = "Stop";
+                        dirBox.Enabled = false;
+                    }
                     break;
                 case 1: //Watching but not processing or checking
                     //We can just stop the watcher straight away
@@ -149,16 +167,16 @@ namespace Doc_to_RTF_Watcher
         }
 
         private void runConversion()
-        {   //Converts all the .doc and .docx files in the folder into RTFs
-
-            //Create an output folder if it doesn't exist
-            if (!Directory.Exists(dirBox.Text + @"\Output"))
-            {
-                Directory.CreateDirectory(dirBox.Text + @"\Output");
-            }
+        {   //Converts all the .doc and .docx files in the folder into 
 
             //First get all the files in the folder that we need to convert
             string[] fileNames = Directory.GetFiles(dirBox.Text, "*.doc*");
+
+            //Pick up and then clear the list of files to convert
+            //string[] fileNames = toConvert.ToArray();
+            Console.WriteLine("In runConversion now. Converting the following files:");
+            Console.WriteLine(fileNames);
+            //toConvert.Clear();
 
             //Now create word and get transforming
             Word.Application wordApp = new Word.Application();
